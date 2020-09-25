be4243b4a7c287bf135b4c765625552c8bf83dfe
Vahid Reza Khazaie <34673511+vrkh1996@users.noreply.github.com>
2020-09-25 17:41:02 +0330

    Update Unet.py

--- Unet.py
+++ Unet.py
@@ -1,9 +1,7 @@
+import os
 import tensorflow as tf
 import time
 import datetime
-import numpy as np
-import os
-
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -13,216 +11,203 @@ if gpus:
         # Virtual devices must be set before GPUs have been initialized
         print(e)
 
+PATH_TRAIN = "LJspeech_spectrograms"
+log_dir = "logs_specs/"
 
-# Load the model
-tf.compat.v1.disable_eager_execution()
-tf.compat.v1.reset_default_graph()
-saver = tf.compat.v1.train.import_meta_graph('infer.meta')
-graph = tf.compat.v1.get_default_graph()
-sess = tf.compat.v1.InteractiveSession()
-saver.restore(sess, 'model.ckpt')
-
-
-
-log_dir = "logs/"
+BUFFER_SIZE = 128
 BATCH_SIZE = 64
-STEPS = 20000
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+CHANNELS = 3
+EPOCHS = 250
 
 
-def generate_spectrograms(ngenerate=64, set_random=True, latent=None):
-    z = graph.get_tensor_by_name('z:0')
-    G_z = graph.get_tensor_by_name('G_z:0')[:, :, 0]
-    G_z_spec = graph.get_tensor_by_name('G_z_spec:0')
+def load(image_file):
+    input_image = tf.io.read_file(image_file)
+    input_image = tf.image.decode_jpeg(input_image)
+    input_image = tf.cast(input_image, tf.float32)
 
-    if set_random:
-        # Sample latent vectors
-        _z = (np.random.rand(ngenerate, 100) * 2.) - 1.
-
-        # Generate
-        _G_z, _G_z_spec = sess.run([G_z, G_z_spec], {z: _z})
-
-    else:
-        # Generate
-        _G_z, _G_z_spec = sess.run([G_z, G_z_spec], {z: latent})
+    return input_image
 
 
-    spectrograms = np.expand_dims(np.array(_G_z_spec), axis=3).astype("float32") / 255.
+def resize(input_image, height, width):
+    input_image = tf.image.resize(input_image, [height, width],
+                                  method=tf.image.ResizeMethod.BICUBIC)
 
-    return spectrograms
+    return input_image
 
+
+# normalizing the images to [0, 1]
+def normalize(input_image):
+    return input_image / 255.
+
+
+def load_image_train(image_file):
+    input_image = load(image_file)
+    face_normalized = normalize(resize(input_image, IMG_HEIGHT, IMG_WIDTH))
+
+    return face_normalized
+
+
+def load_image_test(image_file):
+    input_image = load(image_file)
+    face_normalized = normalize(resize(input_image, IMG_HEIGHT, IMG_WIDTH))
+
+    return face_normalized
+
+
+def downsample(filters, size, strides=2, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2D(filters, size, strides=strides, padding='same',
+                               kernel_initializer=initializer, use_bias=False))
+
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
+
+
+def upsample(filters, size, strides=2, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filters, size, strides=strides,
+                                        padding='same',
+                                        kernel_initializer=initializer,
+                                        use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+    return result
 
 
 def UNet():
-    # Encoder
-    inputs = tf.keras.layers.Input(shape=[256, 256, 1])
+    inputs = tf.keras.layers.Input(shape=[IMG_WIDTH, IMG_HEIGHT, CHANNELS])
 
-    enc_1 = tf.keras.layers.Conv2D(filters=64, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(inputs)
-    enc_1 = tf.keras.layers.LeakyReLU()(enc_1)
+    down_stack = [
+        downsample(64, 4, apply_batchnorm=False),  # (bs, 64, 64, 64)
+        downsample(128, 4),  # (bs, 32, 32, 128)
+        downsample(256, 4),  # (bs, 16, 16, 256)
+        downsample(512, 4),  # (bs, 8, 8, 512)
+        downsample(512, 4),  # (bs, 4, 4, 512)
+        downsample(512, 4),  # (bs, 2, 2, 512)
+        downsample(512, 4),  # (bs, 1, 1, 512)
+    ]
 
-    enc_2 = tf.keras.layers.Conv2D(filters=128, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_1)
-    enc_2 = tf.keras.layers.BatchNormalization()(enc_2)
-    enc_2 = tf.keras.layers.LeakyReLU()(enc_2)
+    up_stack = [
+        upsample(512, 4, apply_dropout=True),  # (bs, 2, 2, 1024)
+        upsample(512, 4, apply_dropout=True),  # (bs, 4, 4, 1024)
+        upsample(512, 4, apply_dropout=True),  # (bs, 8, 8, 1024)
+        upsample(512, 4),  # (bs, 16, 16, 1024)
+        upsample(256, 4),  # (bs, 32, 32, 512)
+        upsample(128, 4),  # (bs, 64, 64, 256)
+        upsample(64, 4),  # (bs, 128, 128, 128)
+    ]
 
-    enc_3 = tf.keras.layers.Conv2D(filters=256, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_2)
-    enc_3 = tf.keras.layers.BatchNormalization()(enc_3)
-    enc_3 = tf.keras.layers.LeakyReLU()(enc_3)
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = tf.keras.layers.Conv2DTranspose(CHANNELS, 4,
+                                           strides=2,
+                                           padding='same',
+                                           kernel_initializer=initializer,
+                                           activation='relu')  # (bs, 128, 128, 3)
 
-    enc_4 = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_3)
-    enc_4 = tf.keras.layers.BatchNormalization()(enc_4)
-    enc_4 = tf.keras.layers.LeakyReLU()(enc_4)
+    x = inputs
 
-    enc_5 = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_4)
-    enc_5 = tf.keras.layers.BatchNormalization()(enc_5)
-    enc_5 = tf.keras.layers.LeakyReLU()(enc_5)
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
 
-    enc_6 = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_5)
-    enc_6 = tf.keras.layers.BatchNormalization()(enc_6)
-    enc_6 = tf.keras.layers.LeakyReLU()(enc_6)
+    skips = reversed(skips[:-1])
 
-    enc_7 = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_6)
-    enc_7 = tf.keras.layers.BatchNormalization()(enc_7)
-    enc_7 = tf.keras.layers.LeakyReLU()(enc_7)
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = tf.keras.layers.Concatenate()([x, skip])
 
-    enc_8 = tf.keras.layers.Conv2D(filters=512, kernel_size=4, strides=2, padding='same',
-                                   kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(enc_7)
-    enc_8 = tf.keras.layers.BatchNormalization()(enc_8)
-    enc_8 = tf.keras.layers.LeakyReLU()(enc_8)
+    x = last(x)
 
-    # latent space
-    latent = tf.keras.layers.Reshape((512,))(enc_8)
-    latent = tf.keras.layers.Dense(100, activation='tanh')(latent)
-    dec_1 = tf.keras.layers.Dense(512)(latent)
-    dec_1 = tf.keras.layers.Reshape((1, 1, 512))(dec_1)
-    dec_1 = tf.keras.layers.BatchNormalization()(dec_1)
-    dec_1 = tf.keras.layers.LeakyReLU()(dec_1)
-
-    # Decoder
-    dec_1 = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_1)
-    dec_1 = tf.keras.layers.BatchNormalization()(dec_1)
-    dec_1 = tf.keras.layers.Dropout(0.5)(dec_1)
-    dec_1 = tf.keras.layers.ReLU()(dec_1)
-    dec_1 = tf.keras.layers.Concatenate()([dec_1, enc_7])
-
-    dec_2 = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_1)
-    dec_2 = tf.keras.layers.BatchNormalization()(dec_2)
-    dec_2 = tf.keras.layers.Dropout(0.5)(dec_2)
-    dec_2 = tf.keras.layers.ReLU()(dec_2)
-    dec_2 = tf.keras.layers.Concatenate()([dec_2, enc_6])
-
-    dec_3 = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_2)
-    dec_3 = tf.keras.layers.BatchNormalization()(dec_3)
-    dec_3 = tf.keras.layers.Dropout(0.5)(dec_3)
-    dec_3 = tf.keras.layers.ReLU()(dec_3)
-    dec_3 = tf.keras.layers.Concatenate()([dec_3, enc_5])
-
-    dec_4 = tf.keras.layers.Conv2DTranspose(filters=512, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_3)
-    dec_4 = tf.keras.layers.BatchNormalization()(dec_4)
-    dec_4 = tf.keras.layers.Dropout(0.5)(dec_4)
-    dec_4 = tf.keras.layers.ReLU()(dec_4)
-    dec_4 = tf.keras.layers.Concatenate()([dec_4, enc_4])
-
-    dec_5 = tf.keras.layers.Conv2DTranspose(filters=256, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_4)
-    dec_5 = tf.keras.layers.BatchNormalization()(dec_5)
-    dec_5 = tf.keras.layers.ReLU()(dec_5)
-    dec_5 = tf.keras.layers.Concatenate()([dec_5, enc_3])
-
-    dec_6 = tf.keras.layers.Conv2DTranspose(filters=128, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_5)
-    dec_6 = tf.keras.layers.BatchNormalization()(dec_6)
-    dec_6 = tf.keras.layers.ReLU()(dec_6)
-    dec_6 = tf.keras.layers.Concatenate()([dec_6, enc_2])
-
-    dec_7 = tf.keras.layers.Conv2DTranspose(filters=64, kernel_size=4, strides=2, padding='same',
-                                            kernel_initializer=tf.random_normal_initializer(0., 0.02), use_bias=False)(
-        dec_6)
-    dec_7 = tf.keras.layers.BatchNormalization()(dec_7)
-    dec_7 = tf.keras.layers.ReLU()(dec_7)
-    dec_7 = tf.keras.layers.Concatenate()([dec_7, enc_1])
-
-    last = tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=4, strides=2, padding='same',
-                                           kernel_initializer=tf.random_normal_initializer(0., 0.02),
-                                           activation='relu')(dec_7)
-
-    return tf.keras.Model(inputs=inputs, outputs=[last, latent])
+    return tf.keras.Model(inputs=inputs, outputs=x)
 
 
-def loss(x_spec, x_spec_autoencoded, x_spec_prime):
-    reconstruction_loss = mae(x_spec, x_spec_autoencoded)
-    inverse_loss = mae(x_spec, x_spec_prime)
 
-    return reconstruction_loss + inverse_loss
+def loss(hr_face, sr_face):
+    # MAE between faces in pixel space
+    pixel_loss = mae(hr_face, sr_face)
 
+    return pixel_loss
 
 
 @tf.function
-def fit(steps):
-    for step in range(steps):
+def train_step(hr_face, epoch):
+    with tf.GradientTape() as grad_tape:
+        upsampled = model(hr_face, training=True)
+
+        total_loss = loss(hr_face, upsampled)
+
+        gradients = grad_tape.gradient(total_loss, model.trainable_variables)
+
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        with summary_writer.as_default():
+            tf.summary.scalar('total_loss', total_loss, step=epoch)
+
+
+def fit(train_ds, epochs):
+    for epoch in range(epochs):
         start = time.time()
 
-        print("Step: [", step + 1, '/', steps, ']')
+        print("Epoch: ", epoch + 1)
 
         # Train
-        with tf.GradientTape() as grad_tape:
-            spec = generate_spectrograms(ngenerate=BATCH_SIZE, set_random=True)
-            x_spec_autoencoded, latent = model(spec, training=True)
-            x_spec_prime = generate_spectrograms(ngenerate=BATCH_SIZE, set_random=False, latent=latent)
+        for n, face_normalized in train_ds.enumerate():
+            print(n.numpy() + 1)
 
-            total_loss = loss(spec, x_spec_autoencoded, x_spec_prime)
-
-            gradients = grad_tape.gradient(total_loss, model.trainable_variables)
-
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-            with summary_writer.as_default():
-                tf.summary.scalar('total_loss', total_loss, step=step)
-
-        print('Time taken for this step is {} sec\n'.format(time.time() - start))
+            train_step(face_normalized, epoch)
 
         # saving (checkpoint) the model each epoch
         checkpoint.save(file_prefix=checkpoint_prefix)
 
+        print('Time taken for epoch {} is {} sec\n'.format(epoch + 1, time.time() - start))
 
 
-"""Building the model"""
+"""Input Pipeline"""
+train_dataset = tf.data.Dataset.list_files(PATH_TRAIN + '/*')
+train_dataset = train_dataset.map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
+
+
 model = UNet()
 model.summary()
 
-
 """Define Loss and Optimizer"""
 mae = tf.keras.losses.MeanAbsoluteError()
-optimizer = tf.keras.optimizers.SGD(1e-3, momentum=0.9)
+optimizer = tf.keras.optimizers.SGD(2e-4, momentum=0.9)
 
-
-checkpoint_dir = './checkpoints'
+checkpoint_dir = './checkpoints_specs'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
 checkpoint = tf.train.Checkpoint(optimizer=optimizer,
                                  model=model,
                                  )
-summary_writer = tf.summary.create_file_writer(log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
+summary_writer = tf.summary.create_file_writer(log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
 # restoring the latest checkpoint in checkpoint_dir
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
-
 """Train the Model"""
-fit(STEPS)
-
+fit(train_dataset, EPOCHS)
